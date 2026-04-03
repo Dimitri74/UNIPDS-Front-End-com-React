b720219a974c3415b44fb05e6d3726ed681d287f
Marcus Dimitri <marcusdimitri7@gmail.com>
2026-04-03 18:46:40 -0300

    COMUNICAÇÃO COM APIS (PARTE 1)

--- fundamentos-front-end-react/app/page.tsx
+++ fundamentos-front-end-react/app/page.tsx
@@ -20,6 +20,41 @@ export default function Home() {
               Aula 2 - Nível 2
             </Link>
           </li>
+          <li>
+            <Link className="underline" href="/aula-3/server-side">
+              Aula 3 - Server-side
+            </Link>
+          </li>
+          <li>
+            <Link className="underline" href="/aula-3/client-side">
+              Aula 3 - Client-side
+            </Link>
+          </li>
+          <li>
+            <Link className="underline" href="/aula-3/community-libraries">
+              Aula 3 - Community libraries
+            </Link>
+          </li>
+          <li>
+            <Link className="underline" href="/login">
+              Aula 4 - Login
+            </Link>
+          </li>
+          <li>
+            <Link className="underline" href="/dashboard">
+              Aula 4 - Dashboard
+            </Link>
+          </li>
+          <li>
+            <Link className="underline" href="/test-server">
+              Aula 4 - Test Server-side
+            </Link>
+          </li>
+          <li>
+            <Link className="underline" href="/test-client">
+              Aula 4 - Test Client-side
+            </Link>
+          </li>
         </ol>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
